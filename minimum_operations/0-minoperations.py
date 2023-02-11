e8ad8ee6c65e87af3a5551fc65a73ec00c82a9b2
misskukuua <p.asare@alustudent.com>
2023-02-10 22:48:19 -0800

    minimumoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+finding minimum number of operations on copy and paste
+"""
+
+
+def minOperations(n):
+    """
+    fewest number of operations that result to n and H in the file.
+    """
+
+    if n <= 1:
+        return 0
+
+    for i in range(2, int((n / 2) + 1)):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
+    return n
